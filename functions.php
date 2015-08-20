25f23891c4ec20f8e347e922f9c485c8443177e0
celsowhite <celso.white13@gmail.com>
2015-08-20 13:14:58 -0400

    Add custom login screen functionality and styles.

--- functions.php
+++ functions.php
@@ -13,3 +13,21 @@ function wp_load_files() {
 }
 
 add_action('wp_enqueue_scripts', 'wp_load_files', 11);
+
+/*=============================================
+CUSTOM LOGIN SCREEN
+=============================================*/
+
+// Change the login logo URL
+
+function my_loginURL() {
+    return 'http://loginurl.com';
+}
+add_filter('login_headerurl', 'my_loginURL');
+
+// Enque the login specific stylesheet for design customizations. CSS file is compiled through compass.
+
+function my_logincustomCSSfile() {
+    wp_enqueue_style('login-styles', get_stylesheet_directory_uri() . '/css/login.css');
+}
+add_action('login_enqueue_scripts', 'my_logincustomCSSfile');
